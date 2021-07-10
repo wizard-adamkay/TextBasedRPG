adcd8dd5c035f8ce8d8377ea126a22d0f2314caf
Silooh <Danielhongjin@gmail.com>
2021-07-10 02:48:07 -0700

    Changed event decisions to not get added to navigation stack.

--- components/ui/Event.js
+++ components/ui/Event.js
@@ -1,5 +1,5 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import React, { useState } from 'react';
 import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
@@ -7,9 +7,9 @@ import GenericEvents from "./../../data/events/GenericEvents.js";
 
 
 const EventScreen = ({ route, navigation}) => {
-  const data = route.params;
-  console.log(route.params);
-  const eventData = GenericEvents["" + data.eventID];
+  const [eventID, setEventID] = useState(route.params.eventID);
+
+  const eventData = GenericEvents["" + eventID];
   return (
     <View style={styles.container}>
       <Text  style={{borderRadius: 5, width: '75%', height: 170, backgroundColor: 'green', padding: 2}} title="Event Description"> 
@@ -17,7 +17,8 @@ const EventScreen = ({ route, navigation}) => {
       </Text>
       <br></br>
       {eventData.decisions.map((decision) => {
-        return (<TouchableOpacity  style={{borderRadius: 5, width: '50%', height: 70, backgroundColor: 'dodgerblue', padding: 2, marginTop: 20}} title="New Game" onPress={() => navigation.push('Event', {eventID: "donut02"})}> 
+        return (
+        <TouchableOpacity  style={{borderRadius: 5, width: '50%', height: 70, backgroundColor: 'dodgerblue', padding: 2, marginTop: 20}} title="New Game" onPress={() => setEventID("donut02")}> 
           <Text>{decision.text}</Text>
       </TouchableOpacity>);
       })}
